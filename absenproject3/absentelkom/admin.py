f5a7f803a82072515ebaad501ef41c6602d026c3
SwingsNFI <59271780+SwingsNFI@users.noreply.github.com>
2020-07-04 22:16:56 +0700

    Create admin.py

---
+++ absenproject3/absentelkom/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Tbl_siswa)
+admin.site.register(Tbl_guru)
+admin.site.register(Tbl_kelas)
+admin.site.register(Tbl_akses)
+admin.site.register(Tbl_validasi)
+admin.site.register(Tbl_absen)
+admin.site.register(Tbl_acara)
+admin.site.register(Tbl_jampel_start)
+admin.site.register(Tbl_jampel_end)
+admin.site.register(Tbl_kategori)
+admin.site.register(Tbl_absen_ami)
